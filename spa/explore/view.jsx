675169736851ee1cc8bba939c53b0b1fa43ec88c
alet89 <alessandrotoschi@me.com>
2020-10-26 10:46:25 +0100

    changes

--- spa/explore/view.jsx
+++ spa/explore/view.jsx
@@ -1,8 +1,19 @@
 var Explore = React.createClass({
     render() {
         return(
-        <section>
-            
+        <section className="Pager">
+            <section className="collections">
+                <section className="collection">
+                    <figure className="collectionIcon">
+                        <img></img>
+                    </figure>
+                    <article className="collectionInfo">
+                        <h3 className="collectionTitle" className="BrandizedS">Collection Name</h3>
+                        <p className="collectionDesc">Collection Description Collection Description Collection Description Collection Description Collection Description Collection Description Collection Description Collection Description Collection Description v Collection Description Collection Description Collection Description Collection Description Collection Description Collection Description</p>
+                        <span className="collectionItems">3 ITEMS</span>
+                    </article>
+                </section>
+            </section>
         </section>);
     }
 });
