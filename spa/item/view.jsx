9ee0f7bdb2642d129340fc33911a86ff7411170f
alet89 <alessandrotoschi@me.com>
2020-10-27 22:01:50 +0100

    r

--- spa/item/view.jsx
+++ spa/item/view.jsx
@@ -86,46 +86,57 @@ var Item = React.createClass({
                             <LazyImageLoader src={item.image} />
                         </figure>
                         <article className="itemInfo">
-                            <h3 className="ItemTitle" className="BrandizedS">{item.name}</h3>
-                            <section className="itemaddress">
-                                <h5 className="itemaddressnft">NFT: <a target="_blank" href={window.context.openSeaItemLinkTemplate.format(this.props.collection.address, item.objectId)}>{window.shortenWord(item.objectId, 9)}</a></h5>
-                                <h5 className="itemaddress20">ERC20: <a target="_blank" href={`${window.getNetworkElement("etherscanURL")}address/${item.address}`}>{window.shortenWord(item.address, 10)}</a></h5>
+                            <section className="itemFundamentals">
+                                <h3 className="ItemTitle">{item.name} <span> ({this.props.collection.symbol})</span></h3>
+                                <section className="itemFundamentalsThing">
+                                    <h5 className="itemaddress"><a target="_blank" href={window.context.openSeaItemLinkTemplate.format(this.props.collection.address, item.objectId)}>NFT Address</a></h5>
+                                    <h5 className="itemaddress"><a target="_blank" href={`${window.getNetworkElement("etherscanURL")}address/${item.address}`}>ERC20 Address</a></h5>
+                                </section>
+                                <span className="ItemCollectionLink">wimd.item.eth.link</span>
+                                <section className="itemFundamentalsThing">
+                                    <span className="ItemSupply">Supply: {window.fromDecimals(item.dynamicData.totalSupply, item.decimals)} </span>
+                                    {window.walletAddress && item.dynamicData && item.dynamicData.balanceOf && item.dynamicData.balanceOf !== '0' && <span className="ItemBalance">| You own: {window.fromDecimals(item.dynamicData.balanceOf, item.decimals)}</span>}
+                                </section>
                             </section>
-                            <p className="itemDesc">{window.shortenWord(item.description, 100)}</p>
-                            <a className="ItemCollectionLink" target="_blank" href={window.context.openSeaCollectionLinkTemplate.format(this.props.collection.openSeaName)}>Collection: {this.props.collection.symbol}</a>
-                            <a className="ItemCollectionLink">Link: wimd.item.eth.link</a>
+                            <p className="itemDesc">{window.shortenWord(item.description, 1000)}</p>
                             <section className="itemSide">
-                                <span className="ItemSupply">Quantity: {window.fromDecimals(item.dynamicData.totalSupply, item.decimals)}</span>
-                                {window.walletAddress && item.dynamicData && item.dynamicData.balanceOf && item.dynamicData.balanceOf !== '0' && <span className="ItemBalance">Balance: {window.fromDecimals(item.dynamicData.balanceOf, item.decimals)}</span>}
-                                <span className="ItemPrice">&#129412; $ {item.dynamicData.tokenPriceInDollarsOnUniswap ? window.formatMoney(item.dynamicData.tokenPriceInDollarsOnUniswap, 1) : "--"} <a target="_blank" href={window.context.uniswapSpawUrlTemplate.format(item.address)}>Swap</a><a target="_blank" href={window.context.uniswapInfoUrlTemplate.format(item.address)}>Info</a></span>
-                                <span className="ItemPrice">&#9973; $ {item.dynamicData.tokenPriceInDollarsOnOpenSea ? window.formatMoney(item.dynamicData.tokenPriceInDollarsOnOpenSea, 1) : "--"} <a target="_blank" href={window.context.openSeaItemLinkTemplate.format(this.props.collection.address, item.objectId)}>Info</a></span>
+                                <a className="ItemPrice" target="_blank" href={window.context.uniswapSpawUrlTemplate.format(item.address)}>&#129412; $ {item.dynamicData.tokenPriceInDollarsOnUniswap ? window.formatMoney(item.dynamicData.tokenPriceInDollarsOnUniswap, 1) : "--"}</a>
+                                <a className="ItemPrice" target="_blank" href={window.context.uniswapInfoUrlTemplate.format(item.address)}>&#128039; Info</a>
+                                <a className="ItemPrice" target="_blank" href={window.context.openSeaItemLinkTemplate.format(this.props.collection.address, item.objectId)}>&#9973; $ {item.dynamicData.tokenPriceInDollarsOnOpenSea ? window.formatMoney(item.dynamicData.tokenPriceInDollarsOnOpenSea, 1) : "--"}</a>
                             </section>
                         </article>
                     </section>
+                    <section className="collectionNav">
+                    <ul>
+                        <li>
+                            <a>METADATA</a>
+                        </li>
+                        <li>
+                            <a>CODE</a>
+                        </li>
+                        <li>
+                            <a>FARM</a>
+                        </li>
+                        <li>
+                            <a>ARBITRAGE</a>
+                        </li>
+                    </ul>
+                    </section>
                     <section className="ItemStuff">
                         <section className="ItemFarm">
-                            <section className="ItemStuffOpen">
-                                <h4 className="BrandizedS">FARM</h4>
-                                Soon @UniFi
-                            </section>
+                            Soon @UniFi
                         </section>
                         <section className="ItemArbitrate">
-                            <section className="ItemStuffOpen">
-                                <h4 className="BrandizedS">ARBITRAGE</h4>
-                                Soon @UniFi
-                            </section>
+                            Soon @UniFi
                         </section>
                         {item.metadata && <section className="ItemData">
                             <section className="ItemStuffOpen">
-                                <h4 className="BrandizedS">METADATA</h4>
                             </section>
                             {this.renderMetadata()}
                             {this.renderMetadata(true)}
                         </section>}
                         <section className="ItemCode">
-                            <section className="ItemStuffOpen">
-                                <h4 className="BrandizedS">CODE</h4>
-                            </section>
+                            
                         </section>
                     </section>
                 </section>
--- spa/wrap/view.jsx
+++ spa/wrap/view.jsx
@@ -6,7 +6,13 @@ var Wrap = React.createClass({
                 <section className="wrapSystem">
                     <section className="wrapBox">
                         <section className="WrapWhat">
-                            
+                            <input className="addressWrapSelector" type="text">ERC20 721 1155 address</input>
+                        </section>
+                        <section className="WrapWhat">
+                            <h6 className="tokenSelectedToWrap">Penguin (WIMD)</h6>
+                            <span className="tokenSelectedToWrapBalance" type="text">balance: 1,000</span>
+                            <input className="BalancetoWrapSelector" type=""></input>
+                            <a>Accept</a><a className="WrapToITEM">ITEMIZE</a>
                         </section>
                     </section>
                 </section>
