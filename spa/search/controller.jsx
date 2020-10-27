ebc9ebc107e86b0b6f9db53772f1af77c249ab97
alet89 <alessandrotoschi@me.com>
2020-10-28 00:40:19 +0100

    sss

---
+++ spa/search/controller.jsx
@@ -0,0 +1,4 @@
+var SearchController = function (view) {
+    var context = this;
+    context.view = view;
+};
---
+++ spa/search/view.jsx
@@ -0,0 +1,15 @@
+var Search = React.createClass({
+    render() {
+        return(
+        <section className="Pager">
+            <section className="searchPage">
+                <section className="searchPageCollection">
+                    <h2>COLLECTIONS</h2>
+                </section>
+                <section className="searchPageITEMS">
+                    <h2>ITEMS</h2>
+                </section>
+            </section>
+        </section>);
+    }
+});
