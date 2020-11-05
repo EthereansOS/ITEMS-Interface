97a7039596775410e135a54ec2acc27411d4558e
alet89 <alessandrotoschi@me.com>
2020-11-05 03:03:24 +0100

    gg

--- spa/successPage/view.jsx
+++ spa/successPage/view.jsx
@@ -1,7 +1,35 @@
 var SuccessPage = React.createClass({
     render() {
-        return (<section>
-            Success!
+        return (<section className="Pager yeahPage">
+            {/* After creating a Collection */}
+            <section className="AllDoneBro">
+                <h2>*Nome* is now a real thing!</h2>
+                <figure>
+                    <img></img>
+                </figure>
+                <p>Spread the word using this link <a></a></p>
+                <h2>And now?</h2>
+                <a className="Enter">Create the first Item</a>
+                <p><a>Go to the Collection page</a></p>
+            </section>
+            {/* After creating an ITEM */}
+            <section className="AllDoneBro">
+                <h2>You have successfully created this amazing new ITEM!</h2>
+                <figure>
+                    <img></img>
+                </figure>
+                <p>Spread the word using this link <a></a></p>
+                <h2>And now?</h2>
+                <a className="Enter">Create another Item</a>
+                <p><a>Go to the ITEM page</a></p>
+            </section>
+            {/* After wrapping an ITEM */}
+            <section className="AllDoneBro">
+                <h2>You have successfully wrapped *Quantity* *Nome*</h2>
+                <h2>And now?</h2>
+                <a className="Enter">Wrap another Item</a>
+                <p><a>Go to the ITEM page</a></p>
+            </section>
         </section>);
     }
 });
