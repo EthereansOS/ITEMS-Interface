5b9f2aafb2724c9c486d984c05f967ef4b2560f5
alet89 <alessandrotoschi@me.com>
2020-11-09 03:31:50 +0100

    k

--- spa/singleCollection/view.jsx
+++ spa/singleCollection/view.jsx
@@ -42,19 +42,21 @@ var SingleCollection = React.createClass({
                     {this.props.miniature && <h3 className="collectionTitle">{window.shortenWord(this.props.collection.name, 25)} <span>({window.shortenWord(this.props.collection.symbol, 6)})</span></h3>}
                     {!this.props.miniature && window.renderExpandibleElement(!this.props.collection.description ? "No description available" : window.convertTextWithLinksInHTML(this.props.collection.description), <p className="collectionDesc" />)}
                     {this.props.miniature && <p className="collectionDesc">{!this.props.collection.description ? "No description available" : window.shortenWord(this.props.collection.description, 100)}</p>}
-                    {!this.props.miniature && <p>Version: <b>{`${this.props.collection.category}_${this.props.collection.standardVersion}.${this.props.collection.erc20WrappedItemVersion}.${this.props.collection.modelVersion}`}</b></p>}
-                    {/*!this.props.miniature && this.props.collection.problems && this.props.collection.problems.length > 0 && this.renderCollectionProblems(this.props.collection.problems)*/}
+                    <section className="collectionInfoSide">
                     {this.props.showItemsCount && <span className="collectionItems BrandizedS">
                         {(!this.state || (this.state.itemsCount !== 0 && !this.state.itemsCount)) && <InnerLoader />}
                         {this.state && this.state.itemsCount > 0 && `${this.state.itemsCount} ITEMS`}
                     </span>}
-                    {false && this.props.collection.ens && this.props.showLink && <a target="_blank" href={`https://${this.props.collection.ens}.link`} className="collectionLink">{this.props.collection.ens}</a>}
-                    {this.props.showLink && <span className="ItemCollectionLink">Link: <a target="_blank" onClick={window.copyHREF} href={window.getHomepageLink(`?collection=${this.props.collection.address}`)} className="collectionLink">{window.getHomepageLink(`?collection=${this.props.collection.address}`)}</a></span>}
-                    {this.props.collection.external_url && this.props.showLink && <span className="ItemCollectionLink">External URL: <a target="_blank" href={window.formatLink(this.props.collection.external_url)} className="collectionLink">{window.formatLinkForExpose(this.props.collection.external_url)}</a></span>}
-                    {this.props.collection.externalDNS && this.props.showLink && <span className="ItemCollectionLink">External DNS: <a target="_blank" href={window.formatLink(this.props.collection.externalDNS)} className="collectionLink">{window.formatLinkForExpose(this.props.collection.externalDNS)}</a></span>}
-                    {this.props.collection.externalENS && this.props.showLink && <span className="ItemCollectionLink">External ENS: <a target="_blank" href={window.formatLink(this.props.collection.externalENS)} className="collectionLink">{window.formatLinkForExpose(this.props.collection.externalENS)}</a></span>}
-                    {this.props.collection.repoUri && this.props.showLink && <span className="ItemCollectionLink">Repo URI: <a target="_blank" href={window.formatLink(this.props.collection.repoUri)} className="collectionLink">{window.formatLinkForExpose(this.props.collection.repoUri)}</a></span>}
-                    {this.props.collection.licence_url && this.props.showLink && <span className="ItemCollectionLink">Licence URL: <a target="_blank" href={window.formatLink(this.props.collection.licence_url)} className="collectionLink">{window.formatLinkForExpose(this.props.collection.licence_url)}</a></span>}
+                    {!this.props.miniature && <p>Version: <b>{`${this.props.collection.category}_${this.props.collection.standardVersion}.${this.props.collection.erc20WrappedItemVersion}.${this.props.collection.modelVersion}`}</b></p>}
+                    {/*!this.props.miniature && this.props.collection.problems && this.props.collection.problems.length > 0 && this.renderCollectionProblems(this.props.collection.problems)*/}
+                    {this.props.showLink && <span className="ItemCollectionLink"><a target="_blank" onClick={window.copyHREF} href={window.getHomepageLink(`?collection=${this.props.collection.address}`)} className="collectionLink">Copy Link</a></span>}
+                    </section>
+                    <section className="collectionInfoSideLinks">
+                        {this.props.collection.externalDNS && this.props.showLink && <span className="ItemCollectionLink"><a target="_blank" href={window.formatLink(this.props.collection.externalDNS)} className="collectionLink">External DNS</a></span>}
+                        {this.props.collection.externalENS && this.props.showLink && <span className="ItemCollectionLink"><a target="_blank" href={window.formatLink(this.props.collection.externalENS)} className="collectionLink">External ENS</a></span>}
+                        {this.props.collection.repoUri && this.props.showLink && <span className="ItemCollectionLink"><a target="_blank" href={window.formatLink(this.props.collection.repoUri)} className="collectionLink">Repo</a></span>}
+                        {this.props.collection.licence_url && this.props.showLink && <span className="ItemCollectionLink"><a target="_blank" href={window.formatLink(this.props.collection.licence_url)} className="collectionLink">Licence</a></span>}
+                    </section>
                 </article>
             ];
         }
