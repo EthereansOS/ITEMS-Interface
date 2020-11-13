d523a21f713fd2f95e1468c96ac5909b66ede3cb
Marco Vasapollo <ceo@metaring.com>
2020-11-13 16:48:18 +0100

    Menu Module

--- spa/menu/view.jsx
+++ spa/menu/view.jsx
@@ -14,7 +14,7 @@ var Menu = React.createClass({
         e && e.preventDefault && e.preventDefault(true) && e.stopPropagation && e.stopPropagation(true);
         var _this = this;
         _this.setState({ selected: e.currentTarget.dataset.index }, function () {
-            _this.props.onSelection(_this.state.menu[_this.state.selected].module);
+            _this.state.menu[_this.state.selected] && _this.props.onSelection(_this.state.menu[_this.state.selected].module);
         });
     },
     onCollectionAddress(e) {
@@ -27,30 +27,29 @@ var Menu = React.createClass({
             <section className="menuMenu">
                 <a className="logo"><section className="logoimg"></section><span className="BrandizedSSxY"> ETHITEM</span></a>
                 <section className="menuSelections">
-                    {this.state && this.state.menu && this.state.menu.map((it, i) =>
-                        <section className="menuSelection" key={it.module}>
-                            <a href="javascript:;" data-index={i} className={"menuSelection BrandizedSSx" + ((i + "") === (this.state.selected + "") ? " Selected" : "")} onClick={_this.onSelection}>{it.name}</a>
-                        </section>)}
-                        <section className="menuSelection"><a target="_blank" href="/doc.html" className="menuSelection BrandizedSSx">Doc</a></section>
+                    {this.state && this.state.menu && this.state.menu.map((it, i) => <section className="menuSelection" key={it.module}>
+                        <a href="javascript:;" data-index={i} className={"menuSelection BrandizedSSx" + ((i + "") === (this.state.selected + "") ? " Selected" : "")} onClick={_this.onSelection}>{it.name}</a>
+                    </section>)}
+                    <section className="menuSelection">
+                        <a target="_blank" href="/doc.html" className="menuSelection BrandizedSSx">Doc</a>
+                    </section>
                 </section>
                 <section className="search">
                     <input ref={ref => this.searchInput = ref} type="search" data-action="onCollectionAddress" onChange={window.onTextChange} onKeyUp={window.onTextChange}/>
                     <a href="javascript:;" onClick={this.onCollectionAddress}>&#x021B5;</a>
                 </section>
-                {/* <a href="javascript:;" onClick={this.toggle} className="smallMenuTrig">MENU</a>
+                {/*<a href="javascript:;" onClick={this.toggle} className="smallMenuTrig">MENU</a>
                 <section style={{"display" : this.props.show ? "inline-block" : this.state && this.state.menu ? "inline-block" : "none"}} className="smallMenu">
-                    
                     <section className="search">
                         <input ref={ref => this.searchInput = ref} type="search" data-action="onCollectionAddress" onChange={window.onTextChange} onKeyUp={window.onTextChange}/>
                         <a href="javascript:;" onClick={this.onCollectionAddress}>&#x021B5;</a>
                     </section>
                     <section className="menuSelections">
-                        {this.state && this.state.menu && this.state.menu.map((it, i) =>
-                            <section className="menuSelection" key={it.module}>
-                                <a href="javascript:;" data-index={i} className={"menuSelection BrandizedSSx" + ((i + "") === (this.state.selected + "") ? " Selected" : "")} onClick={_this.onSelection}>{it.name}</a>
-                            </section>)}
+                        {this.state && this.state.menu && this.state.menu.map((it, i) => <section className="menuSelection" key={it.module}>
+                            <a href="javascript:;" data-index={i} className={"menuSelection BrandizedSSx" + ((i + "") === (this.state.selected + "") ? " Selected" : "")} onClick={_this.onSelection}>{it.name}</a>
+                        </section>)}
                     </section>
-                        </section>}*/}
+                </section>}*/}
                 {!window.walletAddress && <a className="connectWallet Brandized" href="javascript:;" onClick={window.ethereum.enable}>CONNECT</a>}
                 {window.walletAddress && <a className="connectWallet" href="javascript:;" onClick={() => this.emit('wallet/toggle')}><img src={window.makeBlockie(window.walletAddress)} /><span>{window.shortenWord(window.walletAddress, 5, true)} &#8592;</span></a>}
             </section>);
