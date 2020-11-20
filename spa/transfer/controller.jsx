65c58751da1afe47aacfc289a8d7d893bfb9e4b0
alet89 <alessandrotoschi@me.com>
2020-11-20 01:18:07 +0100

    Start
    Adding to front-end some cool stuff!
    
    - Batch transfer!
    - Wrap button in Item page
    - Selector from existing collections (Wrap erc20, 721, 1155)
    - Selector from existing Native collection you host in create page
    - Selector from exiting ITEMs of a collection in wrap page (erc20, erc1155)

---
+++ spa/transfer/controller.jsx
@@ -0,0 +1,217 @@
+var TransferController = function (view) {
+    var context = this;
+    context.view = view;
+
+    context.getSelectedToken = function getSelectedToken(selectedToken) {
+        return selectedToken || (context.view.state && context.view.state.selectedToken);
+    };
+
+    context.onTokenAddressChange = async function(type, address) {
+        try {
+            address = window.web3.utils.toChecksumAddress(address);
+        } catch(e) {
+            address = undefined;
+        }
+        if(!address) {
+            return context.view.setState({selectedToken : type !== 'ETH' ? null : {
+                type,
+                name : 'Ethereum',
+                symbol : 'ETH',
+                decimals : 18
+            }}, type !== 'ETH' ? undefined : context.refreshData);
+        }
+        var selectedToken = {
+            type,
+            address,
+            contract : window.newContract(window.context.IERC1155ABI, address),
+            erc20Contract : window.newContract(window.context.IERC20ABI, address),
+            erc721Contract : window.newContract(window.context.IERC721ABI, address),
+            message : ""
+        };
+        try {
+            selectedToken.name = await window.blockchainCall(selectedToken.contract.methods.name);
+        } catch(e) {
+            try {
+                selectedToken.message = window.context.specialMessages.wrap[type].filter(it => window.web3.utils.toChecksumAddress(it.address) === address)[0].message;
+            } catch(e) {
+                console.error(e);
+            }
+        }
+        try {
+            selectedToken.symbol = await window.blockchainCall(selectedToken.contract.methods.symbol);
+        } catch(e) {
+        }
+        try {
+            selectedToken.decimals = await window.blockchainCall(selectedToken.contract.methods.decimals);
+        } catch(e) {
+        }
+        context.view.setState({selectedToken}, context.refreshData);
+    };
+
+    context.refreshData = async function refreshData() {
+        context.refreshBalanceOf();
+    };
+
+    context.refreshBalanceOf = async function refreshBalanceOf() {
+        var selectedToken = context.getSelectedToken();
+        if(!selectedToken) {
+            return;
+        }
+        try {
+            selectedToken.decimals = selectedToken.decimals || await window.blockchainCall(selectedToken.contract.methods.decimals, selectedToken.tokenId);
+        } catch(e) {
+
+        }
+        selectedToken.balanceOf = '0';
+        selectedToken.approved = true;
+        if(selectedToken.type === 'ERC20') {
+            await context.refreshBalanceOfERC20(selectedToken);
+        } if(selectedToken.type === 'ETH') {
+            selectedToken.balanceOf = await window.web3.eth.getBalance(window.walletAddress);
+        } else {
+            try {
+                selectedToken.balanceOf = await window.blockchainCall(selectedToken.contract.methods.balanceOf, window.walletAddress, selectedToken.tokenId);
+            } catch(e) {
+            }
+        }
+        selectedToken.balanceOfPlain = context.fromDecimals(selectedToken.balanceOf);
+        context.view.setState({selectedToken});
+    };
+
+    context.fromDecimals = function fromDecimals(value) {
+        var selectedToken = context.getSelectedToken(selectedToken);
+        if(!selectedToken || !value) {
+            return;
+        }
+        if(selectedToken.decimals && window.asNumber(selectedToken.decimals) > 1) {
+            value = window.fromDecimals(value, selectedToken.decimals, true);
+        }
+        return value;
+    };
+
+    context.toDecimals = function toDecimals(value) {
+        var selectedToken = context.getSelectedToken(selectedToken);
+        if(!selectedToken || !value) {
+            return;
+        }
+        if(selectedToken.decimals && window.asNumber(selectedToken.decimals) > 1) {
+            value = window.toDecimals(value, selectedToken.decimals);
+        }
+        return value.split(',').join('');
+    };
+
+    context.refreshBalanceOfERC20 = async function refreshBalanceOfERC20(selectedToken) {
+        selectedToken = context.getSelectedToken(selectedToken);
+        if(!selectedToken) {
+            return;
+        }
+        try {
+            selectedToken.balanceOf = await window.blockchainCall(selectedToken.erc20Contract.methods.balanceOf, window.walletAddress);
+        } catch(e) {
+        }
+        selectedToken.allowance = '0';
+        try {
+            selectedToken.allowance = await window.blockchainCall(selectedToken.erc20Contract.methods.allowance, window.walletAddress, window.currentEthItemERC20Wrapper.options.address);
+        } catch(e) {
+        }
+        selectedToken.approved = parseInt(selectedToken.allowance) >= parseInt(selectedToken.balanceOf);
+    };
+
+    context.performApprove = async function performApprove() {
+        var selectedToken = context.getSelectedToken(selectedToken);
+        if(!selectedToken) {
+            return;
+        }
+        await window.blockchainCall(selectedToken.erc20Contract.methods.approve, window.currentEthItemERC20Wrapper.options.address, await window.blockchainCall(selectedToken.erc20Contract.methods.totalSupply));
+    };
+
+    context.performItemize = async function performItemize() {
+        var selectedToken = context.getSelectedToken(selectedToken);
+        if(!selectedToken) {
+            return;
+        }
+        await window.sleep(window.context.inputTimeout);
+        await context[`performItemize${selectedToken.type}`](selectedToken);
+        await context.view.emit('collections/refresh');
+        var collectionAndItem = await context.recoverCollectionAndItem(selectedToken);
+        collectionAndItem.created = 'wrap';
+        context.view.emit('section/change', 'spa/successPage', collectionAndItem);
+    };
+
+    context.performItemizeETH = async function performItemizeETH(selectedToken) {
+        var value = context.toDecimals(selectedToken.tokenAmount);
+        if(parseInt(value) > parseInt(selectedToken.balanceOf)) {
+            throw "Insufficient amount of tokens to wrap";
+        }
+        await window.blockchainCall(value, window.currentEthItemERC20Wrapper.methods.mintETH);
+        selectedToken.tokenId = await blockchainCall(window.currentEthItemERC20Wrapper.methods.ETHEREUM_OBJECT_ID);
+        selectedToken.wrappedAddress = window.currentEthItemERC20Wrapper.options.address;
+    };
+
+    context.performItemizeERC20 = async function performItemizeERC20(selectedToken) {
+        var value = context.toDecimals(selectedToken.tokenAmount);
+        if(parseInt(value) > parseInt(selectedToken.balanceOf)) {
+            throw "Insufficient amount of tokens to wrap";
+        }
+        await window.blockchainCall(window.currentEthItemERC20Wrapper.methods['mint(address,uint256)'], selectedToken.address, value);
+        selectedToken.tokenId = await blockchainCall(window.currentEthItemERC20Wrapper.methods.object, selectedToken.address);
+        selectedToken.wrappedAddress = window.currentEthItemERC20Wrapper.options.address;
+    };
+
+    context.performItemizeERC1155 = async function performItemizeERC1155(selectedToken) {
+        if(!selectedToken.tokenId || selectedToken.tokenId === "") {
+            throw "Token Id is mandatory";
+        }
+        var value = context.toDecimals(selectedToken.tokenAmount);
+        if(parseInt(value) > parseInt(selectedToken.balanceOf)) {
+            throw "Insufficient amount of tokens to wrap";
+        }
+        var recipient = window.ethItemOrchestrator.options.address;
+        try {
+            var version = (await window.blockchainCall(window.currentEthItemFactory.methods.erc1155WrapperModel))[1];
+            var wrapped = await window.blockchainCall(window.currentEthItemKnowledgeBase.methods.wrapper, selectedToken.address, version);
+            recipient = wrapped && wrapped !== window.voidEthereumAddress ? wrapped : recipient;
+        } catch(e) {
+        }
+        await window.blockchainCall(selectedToken.contract.methods.safeTransferFrom, window.walletAddress, window.ethItemOrchestrator.options.address, selectedToken.tokenId, value, "0x");
+        var version = (await window.blockchainCall(window.currentEthItemFactory.methods.erc1155WrapperModel))[1];
+        selectedToken.wrappedAddress = await window.blockchainCall(window.currentEthItemKnowledgeBase.methods.wrapper, selectedToken.address, version);
+    }
+
+    context.performItemizeERC721 = async function performItemizeERC721(selectedToken) {
+        if(!selectedToken.tokenId || selectedToken.tokenId === "") {
+            throw "Token Id is mandatory";
+        }
+        var owner = window.web3.utils.toChecksumAddress(await window.blockchainCall(selectedToken.erc721Contract.methods.ownerOf, selectedToken.tokenId));
+        var approved = window.web3.utils.toChecksumAddress(await window.blockchainCall(selectedToken.erc721Contract.methods.getApproved, selectedToken.tokenId));
+        if(owner !== window.walletAddress && approved !== window.walletAddress) {
+            throw "You don't have the permissions to transfer this ERC721 NFT";
+        }
+        var recipient = window.ethItemOrchestrator.options.address;
+        try {
+            var version = (await window.blockchainCall(window.currentEthItemFactory.methods.erc721WrapperModel))[1];
+            var wrapped = await window.blockchainCall(window.currentEthItemKnowledgeBase.methods.wrapper, selectedToken.address, version);
+            recipient = wrapped && wrapped !== window.voidEthereumAddress ? wrapped : recipient;
+        } catch(e) {
+        }
+        await window.blockchainCall(selectedToken.erc721Contract.methods.safeTransferFrom, window.walletAddress, recipient, selectedToken.tokenId, "0x");
+        var version = (await window.blockchainCall(window.currentEthItemFactory.methods.erc721WrapperModel))[1];
+        selectedToken.wrappedAddress = await window.blockchainCall(window.currentEthItemKnowledgeBase.methods.wrapper, selectedToken.address, version);
+    };
+
+    context.recoverCollectionAndItem = async function recoverCollectionAndItem(selectedToken) {
+        selectedToken.wrappedAddress = window.web3.utils.toChecksumAddress(selectedToken.wrappedAddress);
+        var collection = await window.loadSingleCollection(selectedToken.wrappedAddress);
+        collection.items = collection.items || {};
+        await window.loadItemData(collection.items[selectedToken.tokenId] = collection.items[selectedToken.tokenId] || {
+            objectId : selectedToken.tokenId,
+            collection
+        });
+        return {
+            collection,
+            item : collection.items[selectedToken.tokenId],
+            name : collection.items[selectedToken.tokenId].name,
+            tokenAmount : selectedToken.tokenAmount || 1
+        }
+    };
+};
---
+++ spa/transfer/view.jsx
@@ -0,0 +1,101 @@
+var Transfer = React.createClass({
+    requiredScripts: [
+        'spa/innerLoader.jsx'
+    ],
+    getDefaultSubscriptions() {
+        return {
+            "ethereum/ping" : this.controller.refreshData
+        }
+    },
+    getSelectedTokenType() {
+        return (this.state && this.state.selectedTokenType) || window.context.supportedWrappedTokens[0];
+    },
+    reloadToken(e) {
+        window.preventItem(e);
+        this.controller.onTokenAddressChange(this.getSelectedTokenType(), this.tokenAddressInput.value);
+    },
+    onTokenTypeChange(e) {
+        window.preventItem(e);
+        var _this = this;
+        this.setState({
+            selectedTokenType: e.currentTarget.value,
+        }, function() {
+            _this.controller.onTokenAddressChange(_this.getSelectedTokenType(), _this.tokenAddressInput && _this.tokenAddressInput.value);
+        });
+    },
+    onChange(e) {
+        window.preventItem(e);
+        var value = e.currentTarget.value;
+        var callback = this[e.currentTarget.dataset.action];
+        var timeVar = e.currentTarget.dataset.action + "Timeout";
+        this[timeVar] && window.clearTimeout(this[timeVar]);
+        this[timeVar] = setTimeout(() => callback(value), window.context.inputTimeout);
+    },
+    onTokenAddressChange(value) {
+        var _this = this;
+        _this.setState({selectedToken : null}, () => _this.controller.onTokenAddressChange(_this.getSelectedTokenType(), value));
+    },
+    onTokenIdChange(value) {
+        if (!this.state || !this.state.selectedToken) {
+            return;
+        }
+        this.state.selectedToken.tokenId = value;
+        this.controller.refreshBalanceOf();
+    },
+    onTokenAmountChange(value) {
+        if (!this.state || !this.state.selectedToken) {
+            return;
+        }
+        this.state.selectedToken.tokenAmount = value;
+    },
+    max(e) {
+        window.preventItem(e);
+        if (!this.state || !this.state.selectedToken || !this.state.selectedToken.balanceOfPlain) {
+            return;
+        }
+        this.tokenAmountInput.value = this.state.selectedToken.tokenAmount = this.state.selectedToken.balanceOfPlain.split(",").join("");
+        this.setState({ selectedToken: this.state.selectedToken });
+    },
+    actionEnd() {
+        this.controller.refreshData()
+    },
+    componentDidMount() {
+        this.controller.onTokenAddressChange(this.getSelectedTokenType(), this.tokenAddressInput && this.tokenAddressInput.value);
+        window.setHomepageLink(`?section=transfer`);
+    },
+    render() {
+        var selectedTokenType = this.getSelectedTokenType();
+        var state = this.state || {};
+        return (<section className="Pager">
+            <section className="wrapPage">
+                <section className="wrapBox">
+                    <section className="WrapWhat">
+                        <p>Transfer Items</p>
+                        <br></br>
+                        <select onChange={this.onTokenTypeChange}>
+                            {window.context.supportedWrappedTokens.map(it => <option key={it} selected={selectedTokenType === it} value={it}>{it}</option>)}
+                        </select>
+                        {selectedTokenType !== 'ETH' && <input ref={ref => this.tokenAddressInput = ref} className="addressWrapSelector" type="text" placeholder="Token address" data-action="onTokenAddressChange" onKeyUp={this.onChange} onChange={this.onChange}/>}
+                        {selectedTokenType !== 'ETH' && <a className="LoadToITEM" href="javascript:;" onClick={this.reloadToken}>Load</a>}
+                    </section>
+                    <section className="WrapWhatLoaded">
+                        {state.selectedToken && (state.selectedToken.name || state.selectedToken.symbol) && <h6 className="tokenSelectedToWrap">{window.shortenWord(state.selectedToken.name, 10)} {state.selectedToken.symbol && state.selectedToken.name ? ` (${window.shortenWord(state.selectedToken.symbol, 10)})` : window.shortenWord(state.selectedToken.symbol, 10)}</h6>}
+                        {state.selectedToken && state.selectedToken.message && <p><b>Please pay attention: </b>{state.selectedToken.message}</p>}
+                        {state.selectedToken && selectedTokenType !== 'ERC20' && selectedTokenType !== 'ETH' && <section className="tokenSelectedToWrapDecide">
+                            <input className="BalancetoWrapSelector" placeholder="Token ID" type="text" data-action="onTokenIdChange" onKeyUp={this.onChange} onChange={this.onChange}/>
+                        </section>}
+                        {state.selectedToken && selectedTokenType !== 'ERC721' && state.selectedToken && state.selectedToken.balanceOfPlain && <span className="tokenSelectedToWrapBalance">balance: {state.selectedToken.balanceOfPlain}</span>}
+                        {state.selectedToken && selectedTokenType !== 'ERC721' && <section className="tokenSelectedToWrapDecide">
+                            <a className="tokenSelectedToWrapBalanceALL" onClick={this.max}>MAX</a>
+                            <input ref={ref => this.tokenAmountInput = ref} className="BalancetoWrapSelector" placeholder="Ammount" type="text" placeholder="0.00" spellcheck="false" autocomplete="off" autocorrect="off" inputmode="decimal" pattern="^[0-9][.,]?[0-9]$" data-action="onTokenAmountChange" onKeyUp={this.onChange} onChange={this.onChange} />
+                        </section>}
+                        {selectedTokenType === 'ERC20' && state.performing !== 'approve' && <a className={"BeforeToWrapToITEM" + (!state.selectedToken || state.selectedToken.approved ? " disabled" : "")} data-action="approve" onClick={window.perform} href="javascript:;">Approve</a>}
+                        {selectedTokenType === 'ERC20' && state.performing === 'approve' && <InnerLoader/>}
+                        {state.performing !== 'itemize' && <a className={"WrapToITEM" + (!state.selectedToken || !state.selectedToken.approved ? " disabled" : "")} data-action="itemize" onClick={window.perform} href="javascript:;">ITEMIZE</a>}
+                        {state.performing === 'itemize' && <InnerLoader/>}
+                    </section>
+                </section>
+            </section>
+        </section>);
+    }
+});
