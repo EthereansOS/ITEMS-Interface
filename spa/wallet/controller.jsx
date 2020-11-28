f28c2721e948187a0cfa74eaad35318f1090209d
Marco Vasapollo <ceo@metaring.com>
2020-11-28 18:54:14 +0100

    TransferBatchEvent

--- spa/wallet/controller.jsx
+++ spa/wallet/controller.jsx
@@ -6,8 +6,12 @@ var WalletController = function (view) {
         context.view.setState({ loaded : false }, () => context.view.emit('wallet/update'));
         var address = context.view.props.collections.map(it => window.web3.utils.toChecksumAddress(it.address));
         var blocks = await window.loadBlockSearchTranches();
-        var topics = [
+        var events = [
             window.web3.utils.sha3("TransferSingle(address,address,address,uint256,uint256)"),
+            window.web3.utils.sha3("TransferBatch(address,address,address,uint256[],uint256[])"),
+        ];
+        var topics = [
+            events,
             [],
             [],
             window.web3.eth.abi.encodeParameter("address", window.walletAddress)
@@ -25,15 +29,23 @@ var WalletController = function (view) {
                 idsOfCollection[log.address] = idsOfCollection[log.address] || {};
                 var collection = context.view.props.collections.filter(it => it.address === log.address)[0];
                 collection.items = collection.items || {};
-                var objectId = window.web3.eth.abi.decodeParameters(["uint256", "uint256"], log.data)[0];
-                if(idsOfCollection[log.address][objectId]) {
-                    continue;
+                var objectIds = [];
+                if(log.topics[0] === events[0]) {
+                    objectIds = [window.web3.eth.abi.decodeParameters(["uint256", "uint256"], log.data)[0]];
                 }
-                idsOfCollection[log.address][objectId] = true;
-                promises.push(window.loadItemData(collection.items[objectId] = collection.items[objectId] || {
-                    objectId,
-                    collection
-                }, collection, context.view));
+                if(log.topics[0] === events[1]) {
+                    objectIds = window.web3.eth.abi.decodeParameters(["uint256[]", "uint256[]"], log.data)[0];
+                }
+                for(var objectId of objectIds) {
+                    if(idsOfCollection[log.address][objectId]) {
+                        continue;
+                    }
+                    idsOfCollection[log.address][objectId] = true;
+                    promises.push(window.loadItemData(collection.items[objectId] = collection.items[objectId] || {
+                        objectId,
+                        collection
+                    }, collection, context.view));
+                }
             }
         }
         await Promise.all(promises);
