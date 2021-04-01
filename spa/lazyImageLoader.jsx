aeeb362c1bfb957cfdf4bdda0b4d94657b9a2ef5
Marco Vasapollo <ceo@metaring.com>
2021-04-01 12:20:51 +0200

    Image loader

--- spa/lazyImageLoader.jsx
+++ spa/lazyImageLoader.jsx
@@ -3,10 +3,16 @@ var LazyImageLoader = React.createClass({
         this.loader && (this.loader.style.display = "none");
         this.image && (this.image.style.display = "inline-block");
     },
+    onError() {
+        this.loader && (this.loader.style.display = "none");
+        this.errorMessage && (this.errorMessage.style.display = "inline-block");
+    },
     render() {
+        var src = this.props.src;
         return (<section>
             <section className="LoadImg" ref={ref => this.loader = ref}><img src="assets/img/loadMonolith.png"></img></section>
-            <img src={window.formatLink(this.props.src)} ref={ref => this.image = ref} style={{display : "none"}} onLoad={this.onLoad}/>
+            <img src={window.formatLink(src)} ref={ref => this.image = ref} style={{display : "none"}} onLoad={this.onLoad} onError={this.onError}/>
+            <p ref={ref => this.errorMessage = ref} style={{display : "none"}}>An error occurred while trying to load <a href={window.formatLink(src)} target="_blank">this image</a></p>
         </section>);
     }
 });
