70a7ede69be2b8cd4f90cf79193b6bc51b17c2cb
Marco Vasapollo <ceo@metaring.com>
2021-04-07 12:33:33 +0200

    Frontend Only

--- util/build-contracts.js
+++
@@ -1,2 +0,0 @@
-var path = require('path');
-module.exports = require('truffle-flattener-wrapper')(path.resolve(__dirname, '..', 'contracts'), path.resolve(__dirname, '..', 'flat')).catch(console.error);
