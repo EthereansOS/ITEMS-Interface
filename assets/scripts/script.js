bd7e2510168ddf8e8d1071a5c925ff5092ccff87
Marco Vasapollo <ceo@metaring.com>
2021-08-06 10:11:59 +0200

    gasPriceRemove

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -399,7 +399,6 @@ window.getSendingOptions = function getSendingOptions(transaction, value) {
             }) : transaction.estimateGas({
                     nonce,
                     from,
-                    gasPrice: window.web3.utils.toWei("13", "gwei"),
                     value,
                     gas: '7900000',
                     gasLimit: '7900000'
